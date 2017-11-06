07101a671fc6b29099d5f60274ab80c049f0b3e3
陈昊天 <chenhaotian93@gmail.com>
2017-11-06 16:24:53 +0800

    + 001 "two sum"

---
+++ 1-two-sum.go
@@ -0,0 +1,16 @@
+package leetcode
+
+// store complement of ``nums`` to ``target``: remains[target-nums[n]] = n
+// solution got when complement remains[target-n] found
+func twoSum(nums []int, target int) []int {
+	remains := make(map[int]int, len(nums))
+	for i, n := range nums {
+		if index, found := remains[n]; found {
+			return []int{index, i}
+		}
+
+		remains[target-n] = i
+	}
+
+	return nil
+}
